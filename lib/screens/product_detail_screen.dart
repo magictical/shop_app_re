270cbfee42e57d83795da1cb670a9bc56a8a5fb4
IL KWON KIM <magictical@gmail.com>
2020-05-21 12:51:13 +0900

    add comment

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -9,7 +9,12 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    final loadedProduct = Provider.of<Products>(context).findById(productId);
+    /* listen: false 일 경우 첫 rebuild 에서 provider의 값을 가져오고
+    이후에는 비활성화 이기 때문에 rebuild하더라도 업데이트 하지 않음
+    데이터의 업데이트가 필요 없는 경우 첫 로드에만 사용하면 좋을 듯 하다. 
+    */
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
