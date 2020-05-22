8bc5f9185bfc0ae74349d955bb5037c43464f1b6
IL KWON KIM <magictical@gmail.com>
2020-05-22 13:33:35 +0900

    use provider for Product obj and fetch data from Product provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:section8_shop_app_re/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem({
-    this.id,
-    this.title,
-    this.imageUrl,
-  });
-
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -21,11 +15,11 @@ class ProductItem extends StatelessWidget {
           onTap: () {
             Navigator.of(context).pushNamed(
               ProductDetailScreen.routeName,
-              arguments: id,
+              arguments: product.id,
             );
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
@@ -33,13 +27,15 @@ class ProductItem extends StatelessWidget {
           backgroundColor: Colors.black87,
           leading: IconButton(
             icon: Icon(
-              Icons.favorite,
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
             ),
-            onPressed: () {},
+            onPressed: () {
+              product.togglefavoriteStatus();
+            },
             color: Theme.of(context).accentColor,
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -19,10 +19,9 @@ class ProductsGrid extends StatelessWidget {
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10),
-      itemBuilder: (ctx, index) => ProductItem(
-        id: products[index].id,
-        title: products[index].title,
-        imageUrl: products[index].imageUrl,
+      itemBuilder: (ctx, index) => ChangeNotifierProvider(
+        builder: (c) => products[index],
+        child: ProductItem(),
       ),
     );
   }
