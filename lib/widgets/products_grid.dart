153505c3dd74f9e761a270bda7da382b9afd65d0
IL KWON KIM <magictical@gmail.com>
2020-05-22 14:30:13 +0900

    add comment for provider's builder, create, value

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -19,8 +19,13 @@ class ProductsGrid extends StatelessWidget {
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10),
-      itemBuilder: (ctx, index) => ChangeNotifierProvider(
-        builder: (c) => products[index],
+      //
+      // builder를 사용할 경우 list나 그리드의 아이템이 재활용될때 데이터의 변화를 반영 못할 수 있다.
+      // 또는 인스턴스가 이미 생성되었을 경우(ex: products)에는 .value를 써주면 된다.
+      // builder(create)는 main.dart 에서 위젯을 새로 생성할경우 사용하면 좋다
+      //
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        value: products[index],
         child: ProductItem(),
       ),
     );
